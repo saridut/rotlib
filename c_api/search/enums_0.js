1aca9dde00f7b2a5bfb2749d4316acd79d9e3f57
Sarit Dutta <saridut@gmail.com>
2026-08-17 18:07:29 +0200

    Updated docs

--- c_api/search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['eulangseq_0',['EulangSeq',['../rotlib_8h.html#ab15e429e965919eb1cc2e5ff4df19b72',1,'rotlib.h']]]
-];
--- c_api/search/groups_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['matrix_0',['Direction cosine matrix',['../group__dcm.html',1,'']]]
-];
