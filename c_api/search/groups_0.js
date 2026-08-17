9d698b2f55d6eddf14adf0ca56183428f26247f0
Sarit Dutta <saridut@gmail.com>
2026-08-17 18:09:19 +0200

    Updt docs

---
+++ c_api/search/groups_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['angle_0',['Axis Angle',['../group__aa.html',1,'']]],
+  ['angles_1',['Euler angles',['../group__euler.html',1,'']]],
+  ['axis_20angle_2',['Axis Angle',['../group__aa.html',1,'']]]
+];
---
+++ python_api/_sources/index.rst.txt
@@ -0,0 +1,9 @@
+rotlib |version|
+====================
+
+Welcome to the API documentation of **rotlib**.
+
+.. toctree::
+   :maxdepth: 2
+
+   rotlib
